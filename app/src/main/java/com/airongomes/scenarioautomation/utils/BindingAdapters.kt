7af19149b80577bfe5b9994aa66dbadfffb8928f
Airon Gomes <airon.gms@gmail.com>
2021-03-01 16:37:11 -0300

    Acrescentado Binding adapter com Glide para exibir imagem de url.

--- app/src/main/java/com/airongomes/scenarioautomation/utils/BindingAdapters.kt
+++ app/src/main/java/com/airongomes/scenarioautomation/utils/BindingAdapters.kt
@@ -1,13 +1,18 @@
 package com.airongomes.scenarioautomation.utils
 
 import android.text.format.DateFormat
+import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import com.airongomes.scenarioautomation.R
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.model.stream.MediaStoreImageThumbLoader
+import com.bumptech.glide.request.RequestOptions
 import java.util.*
 
-// --------- Adapter para Item Project ------------
+/** --------- Adapter para Item Project ------------ */
 @BindingAdapter("projectUser")
 fun TextView.projectUser(userName: String){
     val textString = "${resources.getText(R.string.user)} $userName"
@@ -30,4 +35,17 @@ fun ImageView.projectType(type: ProjectType){
         ProjectType.STORE -> R.drawable.ic_store
         else -> R.drawable.ic_building
     })
+}
+
+/** --------- Adapter para Fragment About ------------ */
+@BindingAdapter("imageAbout")
+fun ImageView.imageAbout(imageUrl: String){
+    val imageUri = imageUrl.toUri().buildUpon().scheme("http").build()
+    Glide.with(context)
+        .load(imageUri)
+        .apply(
+            RequestOptions()
+            .placeholder(R.drawable.animation_loading)
+            .error(R.drawable.ic_broken))
+        .into(this)
 }
