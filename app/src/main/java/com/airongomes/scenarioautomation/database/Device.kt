46a40d51c90e0310bf878d03277eefe9ca78a4ef
Airon Gomes <airon.gms@gmail.com>
2021-03-03 09:32:39 -0300

    Criado entidade de dispositivo.

---
+++ app/src/main/java/com/airongomes/scenarioautomation/database/Device.kt
@@ -0,0 +1,17 @@
+package com.airongomes.scenarioautomation.database
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "device_list_table", foreignKeys = [ForeignKey(entity = Environment::class,
+    parentColumns = arrayOf("environmentId"),
+    childColumns = arrayOf("environmentId"),
+    onDelete = ForeignKey.CASCADE)])
+
+data class Device(
+    @PrimaryKey(autoGenerate = true)
+    val deviceId: Long = 0L,
+    val deviceName: String = "",
+    val environmentId: Long
+)
