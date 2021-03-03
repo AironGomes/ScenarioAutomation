52bc34331d7c4f76e998d5062270bd5da736b321
Airon Gomes <airon.gms@gmail.com>
2021-03-03 09:33:36 -0300

    Implementação da Entidade do ambiente.

---
+++ app/src/main/java/com/airongomes/scenarioautomation/database/Environment.kt
@@ -0,0 +1,17 @@
+package com.airongomes.scenarioautomation.database
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "environment_list_table", foreignKeys = [ForeignKey(entity = Project::class,
+        parentColumns = arrayOf("projectId"),
+        childColumns = arrayOf("projectId"),
+        onDelete = ForeignKey.CASCADE)])
+
+data class Environment(
+        @PrimaryKey(autoGenerate = true)
+        val environmentId: Long = 0L,
+        val environmentName: String = "",
+        val projectId: Long
+)
