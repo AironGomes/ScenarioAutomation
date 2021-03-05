79102e19d1b4796732e2c712df9f29d844e3b00a
Airon Gomes <airon.gms@gmail.com>
2021-03-05 01:10:30 -0300

    Implementação de livedata com uri no viewModel.

--- app/src/main/java/com/airongomes/scenarioautomation/viewModel/NewEnvironmentViewModel.kt
+++ app/src/main/java/com/airongomes/scenarioautomation/viewModel/NewEnvironmentViewModel.kt
@@ -1,6 +1,7 @@
 package com.airongomes.scenarioautomation.viewModel
 
 import android.app.Application
+import android.net.Uri
 import androidx.lifecycle.*
 import com.airongomes.scenarioautomation.database.Environment
 import com.airongomes.scenarioautomation.database.EnvironmentDao
@@ -8,6 +9,7 @@ import com.airongomes.scenarioautomation.database.Project
 import com.airongomes.scenarioautomation.database.ProjectDatabase
 import com.airongomes.scenarioautomation.repository.Repository
 import kotlinx.coroutines.launch
+import java.net.URI
 
 class NewEnvironmentViewModel(
     application: Application,
@@ -25,6 +27,11 @@ class NewEnvironmentViewModel(
     val closeFragment: LiveData<Boolean>
         get() = _closeFragment
 
+    // Livedata Uri da imagem
+    private val _uriImage = MutableLiveData<Uri>()
+    val uriImage: LiveData<Uri>
+        get() = _uriImage
+
     /**
      * Inicializa a livedata environment se existir o ambiente salvo no banco de dados
      */
@@ -37,14 +44,14 @@ class NewEnvironmentViewModel(
     /**
     * Responsável por Salvar os dados do ambiente no banco de dados
     */
-    fun saveEnvironment(environmentName: String, imageUriString: String?) {
-        // Pega a data atual em milissegundos
-        val date = System.currentTimeMillis() // TODO: ATUALIZAR DATA
+    fun saveEnvironment(environmentName: String) {
 
+        val newUriImage = uriImage.value
+        val newUriImageString = newUriImage?.toString()
         if(environment == null) {
             val environmentData = Environment(
                     environmentName = environmentName,
-                    imageUri = imageUriString,
+                    imageUri = newUriImageString,
                     projectId = projectId)
 
             viewModelScope.launch {
@@ -54,7 +61,7 @@ class NewEnvironmentViewModel(
             val environmentData = Environment(
                     environmentId = environment!!.value!!.environmentId,
                     environmentName = environmentName,
-                    imageUri = imageUriString,
+                    imageUri = newUriImageString,
                     projectId = projectId)
 
             viewModelScope.launch {
@@ -62,7 +69,23 @@ class NewEnvironmentViewModel(
             }
         }
 
+        // Atualiza data e hora de modificação do projeto
+        updateDateTime()
+
         _closeFragment.value = true
+    }
+
+    /**
+     * Atualiza data e hora de modificação do projeto
+     */
+    fun updateDateTime() {
+        // Atualiza a data do projeto
+        val dateTime = System.currentTimeMillis() // Pega a data atual em milissegundos
+
+        viewModelScope.launch {
+            repository.updateDateTime(projectId, dateTime)
+        }
+
     }
 
     /**
@@ -70,5 +93,12 @@ class NewEnvironmentViewModel(
      */
     fun closeFragmentObserved() {
         _closeFragment.value = false
+    }
+
+    /**
+     * Definir image_uri
+     */
+    fun setImageUri(uri: Uri) {
+        _uriImage.value = uri
     }
 }
