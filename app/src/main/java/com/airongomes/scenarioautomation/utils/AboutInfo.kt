a03223f4fc0e9b9ec99edc336ee3b742d62eb128
Airon Gomes <airon.gms@gmail.com>
2021-03-05 08:43:13 -0300

    Correção do tipo.

--- app/src/main/java/com/airongomes/scenarioautomation/utils/AboutInfo.kt
+++ app/src/main/java/com/airongomes/scenarioautomation/utils/AboutInfo.kt
@@ -1,5 +1,5 @@
 package com.airongomes.scenarioautomation.utils
 
 data class AboutInfo(
-    val imageUrl: String = "https://www.dropbox.com/s/bpts9puqwnp8f72/Scenario.png?dl=1"
+    val imageUrl: String? = "https://www.dropbox.com/s/bpts9puqwnp8f72/Scenario.png?dl=1"
 )
