1cac817056bca1b583ece1ec6b6f2f3a52d771b1
Airon Gomes <airon.gms@gmail.com>
2021-03-05 01:12:20 -0300

    Atualização no layout e banco de dados.

--- app/src/main/java/com/airongomes/scenarioautomation/viewModel/NewDeviceViewModel.kt
+++ app/src/main/java/com/airongomes/scenarioautomation/viewModel/NewDeviceViewModel.kt
@@ -3,15 +3,13 @@ package com.airongomes.scenarioautomation.viewModel
 import android.app.Application
 import androidx.lifecycle.*
 import com.airongomes.scenarioautomation.database.Device
-import com.airongomes.scenarioautomation.database.DeviceDao
-import com.airongomes.scenarioautomation.database.Environment
-import com.airongomes.scenarioautomation.database.ProjectDatabase
 import com.airongomes.scenarioautomation.repository.Repository
 import kotlinx.coroutines.launch
 
 class NewDeviceViewModel(
     application: Application,
-    val environmentId: Long): AndroidViewModel(application) {
+    val environmentId: Long,
+    val projectId: Long): AndroidViewModel(application) {
 
     // Instância do repositório
     private val repository = Repository(application)
@@ -21,23 +19,35 @@ class NewDeviceViewModel(
     val closeFragment: LiveData<Boolean>
         get() = _closeFragment
 
-
     /**
      * Responsável por Salvar os dados do dispositivo no banco de dados
      */
-    fun saveDevice(deviceName: String) {
-        // Pega a data atual em milissegundos
-        val date = System.currentTimeMillis() // TODO: ATUALIZAR DATA
-
+    fun saveDevice(manufacturer: String, device: String) {
         val deviceData = Device(
-            deviceName = deviceName,
-            environmentId = environmentId)
+                manufacturerName = manufacturer,
+                deviceName = device,
+                environmentId = environmentId)
 
         viewModelScope.launch {
             repository.insertDevice(deviceData)
         }
 
+        // Atualiza data e hora de modificação do projeto
+        updateDateTime()
+
         _closeFragment.value = true
+    }
+
+    /**
+     * Atualiza data e hora de modificação do projeto
+     */
+    fun updateDateTime() {
+        // Atualiza a data do projeto
+        val dateTime = System.currentTimeMillis() // Pega a data atual em milissegundos
+
+        viewModelScope.launch {
+            repository.updateDateTime(projectId, dateTime)
+        }
     }
 
     /**
