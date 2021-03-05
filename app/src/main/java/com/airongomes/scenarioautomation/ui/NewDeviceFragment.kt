8bf70140540f473df2e5bf14fe57c33810eb2b86
Airon Gomes <airon.gms@gmail.com>
2021-03-05 01:12:51 -0300

    Atualização no layout e banco de dados.

--- app/src/main/java/com/airongomes/scenarioautomation/ui/NewDeviceFragment.kt
+++ app/src/main/java/com/airongomes/scenarioautomation/ui/NewDeviceFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -11,13 +13,10 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.airongomes.scenarioautomation.R
-import com.airongomes.scenarioautomation.database.ProjectDatabase
 import com.airongomes.scenarioautomation.databinding.FragmentNewDeviceBinding
-import com.airongomes.scenarioautomation.databinding.FragmentNewEnvironmentBinding
 import com.airongomes.scenarioautomation.viewModel.NewDeviceViewModel
 import com.airongomes.scenarioautomation.viewModel.NewDeviceViewModelFactory
-import com.airongomes.scenarioautomation.viewModel.NewEnvironmentViewModel
-import com.airongomes.scenarioautomation.viewModel.NewEnvironmentViewModelFactory
+import com.google.android.material.snackbar.Snackbar
 
 class NewDeviceFragment: Fragment() {
 
@@ -37,10 +36,39 @@ class NewDeviceFragment: Fragment() {
         // Cria uma instância de database e adiciona o projectDao para viewModel
         val application = requireNotNull(this.activity).application
         arguments = NewDeviceFragmentArgs.fromBundle(requireArguments())
-        val viewModelFactory = NewDeviceViewModelFactory(application, arguments.environmentId)
+        val viewModelFactory = NewDeviceViewModelFactory(application, arguments.environmentId, arguments.projectId)
 
         // Cria instância do DetailProjectViewModel
         viewModel = ViewModelProvider(this, viewModelFactory).get(NewDeviceViewModel::class.java)
+
+        // Spinner para opções
+        val spinner: Spinner = binding.spinner
+
+        // Array Adapter com string Array
+        ArrayAdapter.createFromResource(
+                requireContext(),
+                R.array.options_device,
+                android.R.layout.simple_gallery_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_list_item_checked)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
+
+        // Se o dispositivo não for da Scenario, mostra a tela para inserir dados do dispositivo
+        binding.switchScenario.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                binding.spinner.visibility = View.VISIBLE
+                binding.manufacturerTextField.visibility = View.GONE
+                binding.deviceTextField.visibility = View.GONE
+            }
+            else {
+                binding.spinner.visibility = View.GONE
+                binding.manufacturerTextField.visibility = View.VISIBLE
+                binding.deviceTextField.visibility = View.VISIBLE
+            }
+        }
 
         // ClickListener para botão button_confirm
         binding.buttonConfirm.setOnClickListener {
@@ -55,6 +83,11 @@ class NewDeviceFragment: Fragment() {
         // Observar o Livedata closeFragment
         viewModel.closeFragment.observe(viewLifecycleOwner, Observer {
             if(it == true) {
+                Snackbar.make(
+                        binding.viewGroupId,
+                        resources.getText(R.string.message_saved),
+                        Snackbar.LENGTH_SHORT
+                ).show()
                 callDetailEnvironmentFragment(arguments.environmentId)
                 viewModel.closeFragmentObserved()
             }
@@ -67,16 +100,26 @@ class NewDeviceFragment: Fragment() {
      * Verifica se os campos estão preenchidos e chama saveEnvironment de ViewModel
      */
     private fun saveDevice() {
-        val deviceName = binding.deviceEditText.text.toString()
-
-        if(deviceName.isBlank()) {
-            Toast.makeText(
-                requireContext(),
-                getText(R.string.message_device_name_is_blank),
-                Toast.LENGTH_LONG
-            ).show()
+        if (binding.switchScenario.isChecked) {
+            val manufacturer = "Scenario Automation"
+            val device = binding.spinner.selectedItem.toString()
+            viewModel.saveDevice(manufacturer, device)
         }
-        else viewModel.saveDevice(deviceName)
+        else {
+            val device = binding.deviceEditText.text.toString()
+            val manufacturer = binding.manufacturerEditText.text.toString()
+            if(device.isBlank() || manufacturer.isBlank()) {
+                Toast.makeText(
+                        requireContext(),
+                        getText(R.string.message_device_name_is_blank),
+                        Toast.LENGTH_LONG
+                ).show()
+            }
+            else {
+                viewModel.saveDevice(manufacturer, device)
+            }
+        }
+
     }
 
     /**
@@ -85,4 +128,5 @@ class NewDeviceFragment: Fragment() {
     private fun callDetailEnvironmentFragment(environmentId: Long) {
         this.findNavController().navigate(NewDeviceFragmentDirections.actionNewDeviceFragmentToDetailEnvironmentFragment(environmentId))
     }
+
 }
